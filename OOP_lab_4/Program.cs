4a66319d4bb97342b1d9b0efff6725dd09c57ef6
Victoria Shevliuk <viktoriasvyatova37@gmail.com>
2021-12-21 15:46:27 +0200

    added oop lab4 files

---
+++ OOP_lab_4/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace OOP_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            bool exitFlag = false;
+            string ans;
+            while (!exitFlag)
+            {
+                try
+                {
+                    Console.WriteLine("Chose operation:");
+                    Console.WriteLine("\"0\" - sum 2 complex numbers:");
+                    Console.WriteLine("\"1\" - sum complex number and real:");
+                    Console.WriteLine("\"2\" - substract 2 complex numbers:");
+                    Console.WriteLine("\"3\" - substract complex number from real:");
+                    Console.WriteLine("\"4\" - substract real number from complex:");
+                    Console.WriteLine("\"5\" - multiply 2 complex numbers:");
+                    Console.WriteLine("\"6\" - multiply complex number by real:");
+                    Console.WriteLine("\"7\" - divide 2 complex numbers:");
+                    Console.WriteLine("\"8\" - divide complex number by real:");
+                    Console.WriteLine("\"9\" - divide real by complex number:");
+                    Console.WriteLine("\"e\" - exit:");
+
+                    TComplex result = null, num1, num2;
+                    float real;
+
+                    ans = Console.ReadLine();
+                    switch (ans[0])
+                    {
+                        case '0':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input complex num:");
+                            num2 = new TComplex();
+                            result = num1 + num2;
+                            break;
+                        case '1':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input real num:");
+                            real = float.Parse(Console.ReadLine());
+                            result = num1 + real;
+                            break;
+                        case '2':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input complex num:");
+                            num2 = new TComplex();
+                            result = num1 - num2;
+                            break;
+                        case '3':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input real num:");
+                            real = float.Parse(Console.ReadLine());
+                            result = real - num1;
+                            break;
+                        case '4':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input real num:");
+                            real = float.Parse(Console.ReadLine());
+                            result = num1 - real;
+                            break;
+                        case '5':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input complex num:");
+                            num2 = new TComplex();
+                            result = num1 * num2;
+                            break;
+                        case '6':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input real num:");
+                            real = float.Parse(Console.ReadLine());
+                            result = num1 * real;
+                            break;
+                        case '7':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input complex num:");
+                            num2 = new TComplex();
+                            result = num1 / num2;
+                            break;
+                        case '8':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input real num:");
+                            real = float.Parse(Console.ReadLine());
+                            result = num1 / real;
+                            break;
+                        case '9':
+                            Console.WriteLine("Input complex num:");
+                            num1 = new TComplex();
+                            Console.WriteLine("Input real num:");
+                            real = float.Parse(Console.ReadLine());
+                            result = real / num1;
+                            break;
+                        case 'e':
+                            exitFlag = true;
+                            break;
+                        default:
+                            Console.WriteLine("There is no such option!");
+                            break;
+
+                    }
+                    if (result != null)
+                    {
+                        Console.WriteLine("Result is:");
+                        result.Print();
+                        Console.WriteLine();
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+                finally
+                {
+                    Console.WriteLine("This is finally block!");
+                }
+            }
+        }
+    }
+}
---
+++ OOP_lab_4/TComplex.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab4
+{
+    class TComplex
+    {
+        public float Real { get; set; }
+        public float Imaginary { get; set; }
+
+        public TComplex()
+        {
+            try
+            {
+                Console.WriteLine("input real part:");
+                Real = float.Parse(Console.ReadLine());
+                Console.WriteLine("input imaginary part:");
+                Imaginary = float.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                throw new FormatException("ERROR!\n" + ex.StackTrace);
+            }
+        }
+
+        public TComplex(float real, float imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public TComplex(TComplex complexNum)
+        {
+            if (complexNum == null)
+            {
+                throw new ArgumentNullException(nameof(complexNum));
+            }
+            Real = complexNum.Real;
+            Imaginary = complexNum.Imaginary;
+        }
+
+        public void Print()
+        {
+            string msg;
+            if (Imaginary > 0) msg = $"{Real} + {Imaginary}i";
+            else if (Imaginary < 0) msg = $"{Real} {Imaginary}i";
+            else msg = $"{Real}";
+
+            Console.WriteLine(msg);
+        }
+
+        public static TComplex operator +(TComplex num1, TComplex num2)
+        {
+            if (num1 == null)
+            {
+                throw new ArgumentNullException(nameof(num1));
+            }
+            if (num2 == null)
+            {
+                throw new ArgumentNullException(nameof(num2));
+            }
+            return new TComplex(num1.Real + num2.Real, num1.Imaginary + num2.Imaginary);
+        }
+
+        public static TComplex operator +(float real, TComplex num)
+        {
+            if (num == null)
+            {
+                throw new ArgumentNullException(nameof(num));
+            }
+            return new TComplex(num.Real + real, num.Imaginary);
+        }
+
+        public static TComplex operator +(TComplex num, float real)
+        {
+            return real + num;
+        }
+
+        public static TComplex operator -(TComplex num1, TComplex num2)
+        {
+            if (num1 == null)
+            {
+                throw new ArgumentNullException(nameof(num1));
+            }
+            if (num2 == null)
+            {
+                throw new ArgumentNullException(nameof(num2));
+            }
+            return new TComplex(num1.Real - num2.Real, num1.Imaginary - num2.Imaginary);
+        }
+
+        public static TComplex operator -(float real, TComplex num)
+        {
+            if (num == null)
+            {
+                throw new ArgumentNullException(nameof(num));
+            }
+            return new TComplex(real - num.Real, num.Imaginary);
+        }
+
+        public static TComplex operator -(TComplex num, float real)
+        {
+            return num + (-real);
+        }
+
+        public static TComplex operator *(TComplex num1, TComplex num2)
+        {
+            if (num1 == null)
+            {
+                throw new ArgumentNullException(nameof(num1));
+            }
+            if (num2 == null)
+            {
+                throw new ArgumentNullException(nameof(num2));
+            }
+            return new TComplex(num1.Real * num2.Real - num1.Imaginary * num2.Imaginary,
+                                num1.Real * num2.Imaginary + num2.Real * num1.Imaginary);
+        }
+
+        public static TComplex operator /(TComplex num1, TComplex num2)
+        {
+            if (num1 == null)
+            {
+                throw new ArgumentNullException(nameof(num1));
+            }
+            if (num2 == null)
+            {
+                throw new ArgumentNullException(nameof(num2));
+            }
+            float delimiter = MathF.Pow(num2.Real, 2) + MathF.Pow(num2.Imaginary, 2);
+            if (delimiter == 0)
+            {
+                throw new DivideByZeroException("Division by zero!");
+            }
+            float realPart = (num1.Real * num2.Real + num1.Imaginary * num2.Imaginary) / delimiter;
+            float imaginaryPart = (num1.Imaginary * num2.Real - num1.Real * num2.Imaginary) / delimiter;
+
+            return new TComplex(realPart, imaginaryPart);
+        }
+
+        public static TComplex operator *(float realNum, TComplex num)
+        {
+            if (num == null)
+            {
+                throw new ArgumentNullException(nameof(num));
+            }
+            return new TComplex(num.Real * realNum, num.Imaginary * realNum);
+        }
+
+        public static TComplex operator *(TComplex num, float realNum)
+        {
+            return realNum * num;
+        }
+
+        public static TComplex operator /(TComplex num, float real)
+        {
+            if (num == null)
+            {
+                throw new ArgumentNullException(nameof(num));
+            }
+            if (real == 0)
+            {
+                throw new DivideByZeroException("Division by zero!");
+            }
+            return new TComplex(num.Real / real, num.Imaginary / real);
+        }
+
+        public static TComplex operator /(float real, TComplex num)
+        {
+            if (num == null)
+            {
+                throw new ArgumentNullException(nameof(num));
+            }
+            float delimiter = MathF.Pow(num.Real, 2) + MathF.Pow(num.Imaginary, 2);
+            if (delimiter == 0)
+            {
+                throw new DivideByZeroException("Division by zero!");
+            }
+            float realPart = (real * num.Real) / delimiter;
+            float imaginaryPart = -(real * num.Imaginary) / delimiter; ;
+
+            return new TComplex(realPart, imaginaryPart);
+        }
+    }
+}
